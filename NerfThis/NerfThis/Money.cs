ce1d88ffcd57d93978749cf205d0151bf89831c4
Abilena <abthomas625@gmail.com>
2022-10-17 14:46:21 -0500

    brain no workie

--- NerfThis/NerfThis/Money.cs
+++ NerfThis/NerfThis/Money.cs
@@ -7,58 +7,71 @@ using System.Threading.Tasks;
 namespace NerfThis
 {
     public class Money
-    {
-    public static void PaymentType(double total)
+    {   
+        public static void PaymentType(double total)
         {
-            Console.WriteLine("How Will you be paying today? Cash,Check or Credit");
-            string payment = Console.ReadLine();
-            if (payment == "Cash")
-            {
-                Console.WriteLine("Great! Paying with cash I see");
-                Console.WriteLine("Please enter the amount of cash you are giving");
-               double cash = double.Parse(Console.ReadLine());
-                if (cash > total)
+            Console.WriteLine("How will you be paying today? Cash,Check or Credit");
+            string payment = Console.ReadLine().ToUpper().Trim();
+
+            bool goAgain = true;
+            while (goAgain == true)
+            {            
+                try
                 {
-                    double change = cash - total;
-                    Console.WriteLine("Heres your change!" + change);
+                    if (payment == "CASH")
+                    {
+                        Console.WriteLine("Great! Paying with cash I see");
+                        Console.WriteLine("Please enter the amount of cash you are giving");
+                        double cash = double.Parse(Console.ReadLine());
+                        if (cash > total)
+                        {
+                            double change = cash - total;
+                            Console.WriteLine("Heres your change! " + change);
+                        }
+                        else if (cash < total)
+                        {
+                            total -= cash;
+                            Console.WriteLine("You still owe money. " + total);
+                            PaymentType(total);
+
+                        }
+                        else if(cash == total)
+                        {
+                            Console.WriteLine("Thank you for paying exact change!");
+                        }
+                    }
+                    else if (payment == "CHECK")
+                    {
+                        Console.WriteLine("Perfect! Paying with Check");
+                        Console.WriteLine("What is the check number?");
+                        string checkNumber = Console.ReadLine();
+                        Console.WriteLine("Thank");
+                    }
+                    else if (payment == "CREDIT")
+                    {
+                        Console.WriteLine("Nice! Paying with credit.");
+
+                        Console.WriteLine("What is your credit card number?");
+                        string creditNumber = Console.ReadLine();
+
+                        Console.WriteLine("What is your expiration date?");
+                        int expDate = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("and your CVV?");
+                        int securityCode = int.Parse(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.WriteLine("That was not a vaild payment type.");
+                        PaymentType(total);
+                    }
                 }
-                else if (cash < total)
+                catch (FormatException e)
                 {
-                    total -= cash;
-                    Console.WriteLine("You still owe money." + total);
-                    PaymentType(total);
-        
+                    Console.WriteLine("Please enter a valid number.");
+                    goAgain = true;
                 }
-
-            } else if (payment == "Check")
-            {
-                Console.WriteLine("Perfect! Paying with Check");                
-                Console.WriteLine("what is the check number?");
-                string check = Console.ReadLine();
-                Console.WriteLine("thank");
-
-            } else if ( payment == "Credit") {
-                Console.WriteLine("Nice! Paying with credit.");
-              
-                Console.WriteLine("what is your credit card number?");
-                string credit = Console.ReadLine();
-               
-                Console.WriteLine("what is your expiration date?");
-                int credit2 = int.Parse(Console.ReadLine());
-               
-                Console.WriteLine("and your CVV?");
-                int credit3 = int.Parse(Console.ReadLine());
-                
-                Console.WriteLine(credit);
-                Console.WriteLine(credit2);
-                Console.WriteLine(credit3);
-
-
-            } else
-            {
-                Console.WriteLine("Please give a valid answer.");
             }
-      
         }   
     }
 }
