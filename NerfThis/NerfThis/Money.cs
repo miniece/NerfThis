a4f415cbeb7e6664576e2f495a18931181d6a655
miniece <mvelinda@icloud.com>
2022-10-17 16:59:35 -0400

    hi

--- NerfThis/NerfThis/Money.cs
+++ NerfThis/NerfThis/Money.cs
@@ -27,17 +27,19 @@ namespace NerfThis
                         {
                             double change = cash - total;
                             Console.WriteLine("Heres your change! " + change);
+                            break;
                         }
                         else if (cash < total)
                         {
                             total -= cash;
                             Console.WriteLine("You still owe money. " + total);
                             PaymentType(total);
-
+                           
                         }
                         else if(cash == total)
                         {
                             Console.WriteLine("Thank you for paying exact change!");
+                            break;
                         }
                     }
                     else if (payment == "CHECK")
@@ -46,6 +48,7 @@ namespace NerfThis
                         Console.WriteLine("What is the check number?");
                         string checkNumber = Console.ReadLine();
                         Console.WriteLine("Thank");
+                        break;
                     }
                     else if (payment == "CREDIT")
                     {
@@ -59,10 +62,12 @@ namespace NerfThis
 
                         Console.WriteLine("and your CVV?");
                         int securityCode = int.Parse(Console.ReadLine());
+
+                        break;
                     }
                     else
                     {
-                        Console.WriteLine("That was not a vaild payment type.");
+                        Console.WriteLine("That was not a valid payment type.");
                         PaymentType(total);
                     }
                 }
