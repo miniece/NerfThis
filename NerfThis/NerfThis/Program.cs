bfe171dba64c73ed6c7896b1fbf7183df3c0ef2b
miniece <mvelinda@icloud.com>
2022-10-18 13:58:10 -0400

    pls work pls

--- NerfThis/NerfThis/Program.cs
+++ NerfThis/NerfThis/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main()
         {
+            //method that calls the class to run the program
             Store NerfThis = new Store();
 
             Console.WriteLine("Welcome to the NerfThis store. \nWhere you can choose to either slay your enemies and reclaim your seat on the throne \nor have some wholesome outdoor fun :)");
