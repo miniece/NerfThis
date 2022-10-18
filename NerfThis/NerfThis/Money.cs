f43016aa5f430be4a50e2e2e5de2155a8df7c612
Sean <ripper.sean@yahoo.com>
2022-10-18 10:35:48 -0400

    Comments/recepit

--- NerfThis/NerfThis/Money.cs
+++ NerfThis/NerfThis/Money.cs
@@ -8,11 +8,10 @@ namespace NerfThis
 {
     public class Money
     {   
-        public static void PaymentType(double total)
+        public static string PaymentType(double total)
         {
             Console.WriteLine("How will you be paying today? Cash,Check or Credit");
             string payment = Console.ReadLine().ToUpper().Trim();
-
             bool goAgain = true;
             while (goAgain == true)
             {            
@@ -25,13 +24,16 @@ namespace NerfThis
                         double cash = double.Parse(Console.ReadLine());
                         if (cash > total)
                         {
-                            double change = cash - total;
-                            Console.WriteLine("Heres your change! " + change);
+                            double change = Math.Round(cash - total,2);
+                            Console.WriteLine("Heres your change! $" + change);
+                            
                             break;
+                            
                         }
                         else if (cash < total)
                         {
                             total -= cash;
+                            
                             Console.WriteLine("You still owe money. " + total);
                             PaymentType(total);
                            
@@ -40,6 +42,7 @@ namespace NerfThis
                         {
                             Console.WriteLine("Thank you for paying exact change!");
                             break;
+
                         }
                     }
                     else if (payment == "CHECK")
@@ -47,8 +50,9 @@ namespace NerfThis
                         Console.WriteLine("Perfect! Paying with Check");
                         Console.WriteLine("What is the check number?");
                         string checkNumber = Console.ReadLine();
-                        Console.WriteLine("Thank");
+                        
                         break;
+
                     }
                     else if (payment == "CREDIT")
                     {
@@ -63,20 +67,26 @@ namespace NerfThis
                         Console.WriteLine("and your CVV?");
                         int securityCode = int.Parse(Console.ReadLine());
 
+                        
                         break;
+
                     }
                     else
                     {
-                        Console.WriteLine("That was not a valid payment type.");
+                        Console.WriteLine("Haven't you ever been to a STORE BEFORE?!");
                         PaymentType(total);
+
                     }
                 }
                 catch (FormatException e)
                 {
-                    Console.WriteLine("Please enter a valid number.");
+                    Console.WriteLine("(╯°□°）╯︵ ┻━┻  t(-_-t)");
                     goAgain = true;
+                    
                 }
+                
             }
+            return payment;
         }   
     }
 }
